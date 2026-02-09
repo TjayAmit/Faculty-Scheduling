c76c1192f1dfe7e1bd86dba88dccfcef84f8d26d
TjayAmit <tristanjayamit0813@gmail.com>
2026-02-09 16:49:38 +0800

    feat: added user resource and navigation group

--- app/Filament/Resources/Managers/ManagerResource.php
+++ app/Filament/Resources/Managers/ManagerResource.php
@@ -13,12 +13,14 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class ManagerResource extends Resource
 {
     protected static ?string $model = Manager::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string| UnitEnum |null $navigationGroup = 'Profile Management';
 
     protected static ?string $recordTitleAttribute = 'Manager';
 
--- app/Filament/Resources/Subjects/SubjectResource.php
+++ app/Filament/Resources/Subjects/SubjectResource.php
@@ -13,12 +13,14 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class SubjectResource extends Resource
 {
     protected static ?string $model = Subject::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|UnitEnum|null $navigationGroup = "Schedule Management";
 
     protected static ?string $recordTitleAttribute = 'Subject';
 
